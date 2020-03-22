3647e420ed9fbbe3894b99b954ab69d244123117
David <bandadavid531@gmail.com>
2020-03-22 15:43:11 -0500

    formulario peliculas

--- js/form.js
+++ js/form.js
@@ -5,6 +5,6 @@ document.querySelector('#submit').addEventListener('click',function(){
     let idiomas = document.querySelector('#idiomas').value;
     let añadir_cineban = document.querySelector('#añadir_cineban').value;
 
-    let url = "https://api.whatsapp.com/send?phone=593969758158&text=*_CineBan_*%0A*Peliculas*%0A%0A*¿Cual es tu nombre?*%0A" + nombre + "%0A*¿Que pelicula te gustaria que añadieramos?*%0A" + nombre_pelicula + "%0A*¿Idioma que quisieras para la película puesta anteriormente?*%0A" + idiomas + "%0A*¿Que te gustaria que añadieramos a nuestra pagina CineBan?*%0A" + añadir_cineban;
+    let url = "https://api.whatsapp.com/send?phone=593962709111&text=*_CineBan_*%0A*Peliculas*%0A%0A*¿Cual es tu nombre?*%0A" + nombre + "%0A*¿Que pelicula te gustaria que añadieramos?*%0A" + nombre_pelicula + "%0A*¿Idioma que quisieras para la película puesta anteriormente?*%0A" + idiomas + "%0A*¿Que te gustaria que añadieramos a nuestra pagina CineBan?*%0A" + añadir_cineban;
     window.open(url);
 });
